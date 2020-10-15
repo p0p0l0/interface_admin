a03b8b55b617db72789378303320b5a682d2fddd
Thierry LAMUSCATELLA <lamuscatella-thierry@hotmail.fr>
2020-10-15 11:47:58 +0200

    commit correction secu + creation website

--- src/Controller/WebsiteController.php
+++ src/Controller/WebsiteController.php
@@ -5,9 +5,11 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Entity\Client;
 use App\Entity\Website;
+use App\Form\WebsiteType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @Route("/{userId}/{clientId}", name="website_", requirements = {"userId" = "\d+", "clientId" = "\d+"})
@@ -17,14 +19,57 @@ class WebsiteController extends AbstractController
     /**
      * @Route("/website", name="list")
      */
-    public function index(EntityManagerInterface $em, $userId,$clientId)
+    public function index(EntityManagerInterface $em, $userId, $clientId)
     {
         $client = $em->getRepository(Client::class)->find($clientId);
         $user = $em->getRepository(User::class)->find($userId);
         $websites = $em->getRepository(Website::class)->findAll();
+
         return $this->render('website/index.html.twig',[
+            'websites'=>$websites,
             'user'=>$user,
             'client'=>$client
+        ]);
+    }
+
+    /**
+     * @Route("/create", name="create", requirements = {"userId" = "\d+", "clientId" = "\d+"})
+     */
+    public function create(EntityManagerInterface $em, Request $request, $userId, $clientId ){
+       
+        $client = $em->getRepository(Client::class)->find($clientId);
+        $user = $em->getRepository(User::class)->find($userId); 
+
+        $client->setEditAt(new \Datetime())
+               ->setUserEdit($user->getUsername());
+               
+        $website = new Website();
+       
+        $form = $this->createForm(WebsiteType::class,$website);
+        $form->handleRequest($request);
+        
+        $website->setClient($client);
+        $name = $website->getName();
+            
+        if($form->isSubmitted() && $form->isValid()){
+            $em->persist($website);
+            $em->flush();
+
+            $this->addFlash(
+                "success","Le website $name de {$client->getName()} a bien été ajouté "
+            );
+
+            return $this->redirectToRoute('website_list',[
+                'userId'=>$userId,
+                'clientId'=>$clientId
+            ]);
+        }
+
+        return $this->render('website/create.html.twig',[
+            'form'=>$form->createView(),
+            'user'=>$user,
+            'client'=>$client
+
         ]);
     }
 }
---
+++ src/Form/WebsiteType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Website;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class WebsiteType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name',TextType::class,[
+                'label'=>'Nom'
+            ])
+            ->add('url',UrlType::class,[
+                'label'=>'URL'
+            ])
+                
+            ->add('ipServeur',TextType::class,[
+                'label'=>'Adresse du serveur'
+            ])
+            ->add('nameFolder', TextType::class,[
+                'label'=>'Nom du dossier'
+            ])
+            ->add('pathFolder',TextType::class,[
+                'label'=>'Chemin du dossier'
+            ])
+            ->add('valider',SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Website::class,
+        ]);
+    }
+}
